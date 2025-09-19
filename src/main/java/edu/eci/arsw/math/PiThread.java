378d2870fb81210185501118846097019c63310d
laura.rsanchez <laura.rsanchez@mail.escuelaing.edu.co>
2025-09-19 09:28:19 -0500

    feat: Compila, pero solo funciona con los 10 primeros

---
+++ src/main/java/edu/eci/arsw/math/PiThread.java
@@ -0,0 +1,80 @@
+package edu.eci.arsw.math;
+
+public class PiThread extends Thread {
+    private static double Epsilon = 1e-17;
+
+    public static double calculate1(int start){
+
+        double sum = 0;
+
+        sum = 4 * sum(1, start)
+                - 2 * sum(4, start)
+                - sum(5, start)
+                - sum(6, start);
+
+        return sum;
+    }
+
+    /// <summary>
+    /// Returns the sum of 16^(n - k)/(8 * k + m) from 0 to k.
+    /// </summary>
+    /// <param name="m"></param>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    private static double sum(int m, int n) {
+        double sum = 0;
+        int d = m;
+        int power = n;
+
+        while (true) {
+            double term;
+
+            if (power > 0) {
+                term = (double) hexExponentModulo(power, d) / d;
+            } else {
+                term = Math.pow(16, power) / d;
+                if (term < Epsilon) {
+                    break;
+                }
+            }
+
+            sum += term;
+            power--;
+            d += 8;
+        }
+
+        return sum;
+    }
+
+    /// <summary>
+    /// Return 16^p mod m.
+    /// </summary>
+    /// <param name="p"></param>
+    /// <param name="m"></param>
+    /// <returns></returns>
+    private static int hexExponentModulo(int p, int m) {
+        int power = 1;
+        while (power * 2 <= p) {
+            power *= 2;
+        }
+
+        int result = 1;
+
+        while (power > 0) {
+            if (p >= power) {
+                result *= 16;
+                result %= m;
+                p -= power;
+            }
+
+            power /= 2;
+
+            if (power > 0) {
+                result *= result;
+                result %= m;
+            }
+        }
+
+        return result;
+    }
+}
